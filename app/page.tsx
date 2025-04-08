ee19fbc3e4d6f334b3febc90da7e211f02d706ef
risvinmpm <risvinmp39@gmail.com>
2025-04-08 12:34:39 +0530

    navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import { Navbar, NavbarDemo } from "@/components/layout/Navbar";
 import Image from "next/image";
 
 export default function Home() {
   return (
    <>
+   <Navbar />
    </>
   );
 }
