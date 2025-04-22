e6ce3a98066b2b86ceebd8f3c12fcc9a02cd4581
risvinmpm <risvinmp39@gmail.com>
2025-04-22 17:58:32 +0530

    changes

--- pages/Portfolio/Portfolio.tsx
+++ pages/Portfolio/Portfolio.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Portfolio = () => {
   return (
-    <div>Portfolio</div>
+    <div className='main-padding'>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo nulla neque ipsa? Necessitatibus excepturi impedit consequatur dolor dolore. Distinctio iusto nesciunt quia pariatur, nulla voluptatibus a est. Quia, voluptatum temporibus?
+    </div>
   )
 }
 
