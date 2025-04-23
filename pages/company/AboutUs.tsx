2ccab68093844ed727e7ed82ed1133a680e4f3d6
risvinmpm <risvinmp39@gmail.com>
2025-04-23 12:10:47 +0530

    latest changes

--- pages/company/AboutUs.tsx
+++ pages/company/AboutUs.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
+import {Navbar} from "../../components/layout/Navbar";
 
 const AboutUs = () => {
   return (
-    <div>AboutUs</div>
-  )
-}
+    <>
+      <Navbar />
+      <div>AboutUs</div>
+    </>
+  );
+};
 
-export default AboutUs
+export default AboutUs;
