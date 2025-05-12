f38604d145f27b92097fb7b22573e7829be1a077
risvinmpm <risvinmp39@gmail.com>
2025-05-12 12:06:22 +0530

    latest chnages

---
+++ pages/contactus/ContactUs.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Navbar } from '../../components/layout/Navbar'
+import Footer from '../../components/layout/Footer'
+
+const ContactUs = () => {
+  return (
+    <>
+      <Navbar />
+      <main>
+      </main>
+      <Footer />
+    </>
+  )
+}
+
+export default ContactUs
