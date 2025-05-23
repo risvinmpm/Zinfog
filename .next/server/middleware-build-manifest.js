da6f228d97395792893da2894f00e8f7d9cedcb1
risvinmpm <risvinmp39@gmail.com>
2025-05-23 17:20:21 +0530

    changes

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -1,46 +1,6 @@
 globalThis.__BUILD_MANIFEST = {
   "pages": {
-    "/_app": [
-      "static/chunks/node_modules_next_dist_4f3d469a._.js",
-      "static/chunks/node_modules_react-dom_82bb97c6._.js",
-      "static/chunks/node_modules_2a5cf4bb._.js",
-      "static/chunks/[root-of-the-server]__fb6a6365._.js",
-      "static/chunks/styles_globals_79636149.css",
-      "static/chunks/pages__app_5771e187._.js",
-      "static/chunks/pages__app_7132be12._.js"
-    ],
-    "/portfolio/Portfolio": [
-      "static/chunks/[root-of-the-server]__311c37e0._.js",
-      "static/chunks/node_modules_next_081c4778._.js",
-      "static/chunks/node_modules_react-dom_82bb97c6._.js",
-      "static/chunks/node_modules_motion_dist_es_bd906084._.js",
-      "static/chunks/node_modules_tailwind-merge_dist_bundle-mjs_mjs_e33d9f9e._.js",
-      "static/chunks/node_modules_react-icons_hi_index_mjs_5c5deb9e._.js",
-      "static/chunks/node_modules_react-icons_ai_index_mjs_2e83ab32._.js",
-      "static/chunks/node_modules_react-icons_fa_index_mjs_bad01e3f._.js",
-      "static/chunks/node_modules_react-icons_lib_75a63dfe._.js",
-      "static/chunks/node_modules_framer-motion_dist_es_f525d316._.js",
-      "static/chunks/node_modules_motion-dom_dist_es_d48eaee5._.js",
-      "static/chunks/node_modules_396f4f10._.js",
-      "static/chunks/pages_portfolio_Portfolio_tsx_5771e187._.js",
-      "static/chunks/pages_portfolio_Portfolio_tsx_2979ebc0._.js"
-    ],
-    "/product/Product": [
-      "static/chunks/[root-of-the-server]__fa58a8d8._.js",
-      "static/chunks/node_modules_next_e2783ff4._.js",
-      "static/chunks/node_modules_react-dom_82bb97c6._.js",
-      "static/chunks/node_modules_framer-motion_dist_es_9ee989c5._.js",
-      "static/chunks/node_modules_motion-dom_dist_es_d48eaee5._.js",
-      "static/chunks/node_modules_motion_dist_es_bd906084._.js",
-      "static/chunks/node_modules_tailwind-merge_dist_bundle-mjs_mjs_e33d9f9e._.js",
-      "static/chunks/node_modules_react-icons_hi_index_mjs_5c5deb9e._.js",
-      "static/chunks/node_modules_react-icons_ai_index_mjs_2e83ab32._.js",
-      "static/chunks/node_modules_react-icons_fa_index_mjs_bad01e3f._.js",
-      "static/chunks/node_modules_react-icons_lib_75a63dfe._.js",
-      "static/chunks/node_modules_4f85fcd5._.js",
-      "static/chunks/pages_product_Product_tsx_5771e187._.js",
-      "static/chunks/pages_product_Product_tsx_415d92a5._.js"
-    ]
+    "/_app": []
   },
   "devFiles": [],
   "ampDevFiles": [],
